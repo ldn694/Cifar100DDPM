805a713fb21aeea91af11b6e25a1d39c38d1633f
Le Duc Nhuan <nhuanledu694@gmail.com>
2025-09-16 11:48:58 +0700

    Added new sample code

---
+++ data_extract.py
@@ -0,0 +1,43 @@
+import torch
+import torchvision
+import torch.nn.functional as F
+from torchvision import transforms
+import os
+
+image_size = 32
+batch_size = 64
+
+preprocess = transforms.Compose([
+    transforms.Resize((image_size, image_size)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+])
+
+train_dataset = torchvision.datasets.CIFAR100(
+    root="./data", train=True, transform=preprocess, download=True
+)
+
+train_dataloader = torch.utils.data.DataLoader(
+    train_dataset,
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=0,          # you can use >0 safely now
+    pin_memory=True
+)
+
+output_folder = "cifar100_train"
+os.makedirs(output_folder, exist_ok=True)
+
+# Extract 256 images and save them to disk
+image_count = 0
+for i, (images, labels) in enumerate(train_dataloader):
+    for j in range(images.size(0)):
+        if image_count >= 256:
+            break
+        img = images[j]
+        img = (img * 0.5 + 0.5).clamp(0, 1)  # Unnormalize
+        torchvision.utils.save_image(img, f"{output_folder}/image_{image_count:04d}.png")
+        image_count += 1
+    if image_count >= 256:
+        break
--- sample.py
+++ sample.py
@@ -1,24 +1,24 @@
 import torch
 from diffusers import UNet2DModel, DDPMScheduler, DDPMPipeline
 from utils import show_images
+import os
+
+torch.manual_seed(0)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
-model = UNet2DModel.from_pretrained("ddpm-butterflies-32px").to(device)
-noise_scheduler = DDPMScheduler.from_pretrained("ddpm-butterflies-32px")
+model = UNet2DModel.from_pretrained("ddpm-cifar100-32px").to(device)
+noise_scheduler = DDPMScheduler.from_pretrained("ddpm-cifar100-32px")
 image_pipe = DDPMPipeline(unet=model, scheduler=noise_scheduler)
 
-# Random starting point (8 random images):
-sample = torch.randn(8, 3, 32, 32).to(device)
+output_folder = "pipeline_output"
+os.makedirs(output_folder, exist_ok=True)
 
-for i, t in enumerate(noise_scheduler.timesteps):
-
-    # Get model pred
+total_images = 2048
+batch_size = 256
+for i in range(total_images // batch_size):
     with torch.no_grad():
-        residual = model(sample, t).sample
-
-    # Update sample with step
-    sample = noise_scheduler.step(residual, t, sample).prev_sample
-
-show_images(sample).show()
+        output = image_pipe(batch_size).images  # list of PIL images
+    for j, im in enumerate(output):
+        im.save(f"{output_folder}/image_{i * batch_size + j:04d}.png")
